3d170f38746041940d48f9b8f713e5dedcad3724
moguno <moguno@0kn.sakura.ne.jp>
2014-02-10 00:52:31 +0900

    読み込みエラー時に落ちそうなバグを修正

--- sub_parts_image/sub_parts_image.rb
+++ sub_parts_image/sub_parts_image.rb
@@ -77,9 +77,9 @@ Plugin.create :sub_parts_image do
 
         if @image_url
           Gdk::WebImageLoader.get_raw_data(@image_url) {|data| 
-            if data
-              parts_height = UserConfig[:subparts_image_height]
+            parts_height = UserConfig[:subparts_image_height]
 
+            if data
               begin
                 loader = Gdk::PixbufLoader.new
                 loader.write data
